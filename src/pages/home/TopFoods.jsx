4fdd83610aa3cde9d68dd99dde8cfe89a63abbee
omarfaruk-dev <omarfaruk.dev@gmail.com>
2025-06-12 23:34:48 +0600

    text animation added to top food card

--- src/pages/home/TopFoods.jsx
+++ src/pages/home/TopFoods.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import TopFoodsCard from './TopFoodsCard';
 import Spinner from '../shared/Spinner';
+import { Slide } from 'react-awesome-reveal';
 
 const TopFoods = () => {
     const [topFoods, setTopFoods] = useState([]);
@@ -16,12 +17,16 @@ const TopFoods = () => {
     return (
         <div className='max-w-7xl mx-auto px-4 pb-10 md:pb-16 lg:pb-20'>
             <div className="">
-                <h2 className="text-center text-3xl md:text-4xl font-bold text-primary mb-4">
+                <Slide direction='right'>
+                    <h2 className="text-center text-3xl md:text-4xl font-bold text-primary mb-4">
                     Top <span className="text-secondary border-b-2 border-secondary">Foods</span> This Week
                 </h2>
-                <p className="text-center text-accent mb-10 max-w-2xl mx-auto">
+                </Slide>
+                <Slide>
+                    <p className="text-center text-accent mb-10 max-w-2xl mx-auto">
                     Discover the most popular foods, loved by our community! These dishes have the highest purchase counts and are trending right now. Try them and taste why everyone is talking!
                 </p>
+                </Slide>
                 {loading ? (
                     <Spinner/>
                 ) : topFoods.length === 0 ? (
