94b37799647431f795fbbb2a6cb6c73c865a6258
omarfaruk-dev <omarfaruk.dev@gmail.com>
2025-06-17 02:27:09 +0600

    more images added to gallery

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -5,45 +5,84 @@ import "yet-another-react-lightbox/styles.css";
 import { FaSearchPlus } from "react-icons/fa";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Spinner from "../shared/Spinner";
+import img1 from "../../assets/images/gallery/1.jpg";
+import img2 from "../../assets/images/gallery/2.jpg";
+import img3 from "../../assets/images/gallery/3.jpg";
+import img4 from "../../assets/images/gallery/4.jpg";
+import img5 from "../../assets/images/gallery/5.jpg";
+import img6 from "../../assets/images/gallery/6.jpg";
+import img7 from "../../assets/images/gallery/7.jpg";
+import img8 from "../../assets/images/gallery/8.jpg";
+import img9 from "../../assets/images/gallery/9.jpg";
+import img10 from "../../assets/images/gallery/10.jpg";
+import img11 from "../../assets/images/gallery/11.jpg";
+import img12 from "../../assets/images/gallery/12.jpg";
+import img13 from "../../assets/images/gallery/13.jpg";
+import img14 from "../../assets/images/gallery/14.jpg";
+import img15 from "../../assets/images/gallery/15.jpg";
+import img16 from "../../assets/images/gallery/16.jpg";
+import img17 from "../../assets/images/gallery/17.jpg";
+import img18 from "../../assets/images/gallery/18.jpg";
+import img19 from "../../assets/images/gallery/19.jpg";
+import img20 from "../../assets/images/gallery/20.jpg";
+import img21 from "../../assets/images/gallery/21.jpg";
+import img22 from "../../assets/images/gallery/22.jpg";
+import img23 from "../../assets/images/gallery/23.jpg";
+import hero1 from "../../assets/images/hero/hero-1.jpg";
+import hero2 from "../../assets/images/hero/hero-2.jpg";
+import hero3 from "../../assets/images/hero/hero-3.jpg";
+import hero4 from "../../assets/images/hero/hero-4.avif";
+import hero5 from "../../assets/images/hero/hero-5.avif";
+import hero7 from "../../assets/images/hero/hero-7.jpg";
+import hero8 from "../../assets/images/hero/hero-8.jpg";
+import hero9 from "../../assets/images/hero/hero-9.jpg";
 
 const Gallery = () => {
   const images = [
-    "../src/assets/images/gallery/1.jpg",
-    "../src/assets/images/gallery/2.jpg",
-    "../src/assets/images/gallery/3.jpg",
-    "../src/assets/images/gallery/4.jpg",
-    "../src/assets/images/gallery/5.jpg",
-    "../src/assets/images/gallery/6.jpg",
-    "../src/assets/images/gallery/7.jpg",
-    "../src/assets/images/gallery/8.jpg",
-    "../src/assets/images/gallery/9.jpg",
-    "../src/assets/images/gallery/10.jpg",
-    "../src/assets/images/gallery/11.jpg",
-    "../src/assets/images/gallery/12.jpg",
-    "../src/assets/images/gallery/13.jpg",
-    "../src/assets/images/gallery/14.jpg",
-    "../src/assets/images/gallery/15.jpg",
-    "../src/assets/images/gallery/16.jpg",
-    "../src/assets/images/gallery/17.jpg",
-    "../src/assets/images/gallery/18.jpg",
-    "../src/assets/images/gallery/19.jpg",
-    "../src/assets/images/gallery/20.jpg",
-    "../src/assets/images/gallery/21.jpg",
-    "../src/assets/images/gallery/22.jpg",
-    "../src/assets/images/gallery/23.jpg",
-    "../src/assets/images/hero/hero-1.jpg",
-    "../src/assets/images/hero/hero-2.jpg",
-    "../src/assets/images/hero/hero-3.jpg",
-    "../src/assets/images/hero/hero-4.avif",
-    "../src/assets/images/hero/hero-5.avif",
-    "../src/assets/images/hero/hero-7.jpg",
-    "../src/assets/images/hero/hero-8.jpg",
-    "../src/assets/images/hero/hero-9.jpg",
-    "../src/assets/images/gallery/6.jpg",
-    "../src/assets/images/gallery/7.jpg",
-    "../src/assets/images/gallery/8.jpg",
-    "../src/assets/images/gallery/9.jpg",
-    "../src/assets/images/gallery/10.jpg",
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
+    img23,
+    hero1,
+    hero2,
+    hero3,
+    hero4,
+    hero5,
+    hero7,
+    hero8,
+    hero9,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    hero3,
+     img17,
+    img18,
   ];
   const [open, setOpen] = useState(false);
   const [index, setIndex] = useState(0);
